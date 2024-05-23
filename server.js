bf27d514abb245ee07c2a6939fc2fc639b8b1089
ImeneDehimi <imenedehimi8@gmail.com>
2024-05-23 10:16:54 +0100

    ..

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 // imports
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const routes = require("./router/index")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require("./router/index");
 
 const http = require('http');
 const socketIo = require('socket.io');
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-
-app.use(cors())
-
+// Create HTTP server
 const server = http.createServer(app);
 
+// Route
+app.use("/", routes);
+app.get('/', (req, res) => {
+  res.send({ message: "Welcome to the API" });
+});
 
-// route
-app.use("/" ,routes) 
-app.get('/', (req, res)=>{
-  res.send({message : "Welcome to the API"})
-})
-
-// socket
-
-
-const io = require("socket.io")(server, {
+// Socket.IO
+const io = socketIo(server, {
   cors: {
     origin: "https://handyman-home-services.netlify.app",
   },
@@ -35,39 +31,38 @@ const io = require("socket.io")(server, {
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-  // add new User
+  // Add new user
   socket.on("new-user-add", (newUserId) => {
-    // if user is not added previously
+    // If user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
     }
-    // send all active users to new user
+    // Send all active users to new user
     io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
-    // remove user from active users
+    // Remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    // send all active users to all users
+    // Send all active users to all users
     io.emit("get-users", activeUsers);
   });
 
-  // send message to a specific user
+  // Send message to a specific user
   socket.on("send-message", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
     if (user) {
-      io.to(user.socketId).emit("recieve-message", data);
+      io.to(user.socketId).emit("receive-message", data);
     }
   });
 });
 
+// Server initialization
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server running on port ${port}`));
 
-// server init::
-const port = process.env.PORT || 5000
-app.listen(port , ()=> console.log(`server running on port ${port}`))
-
-// connecting to database
+// Connecting to database
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log("Connected to database!"))
