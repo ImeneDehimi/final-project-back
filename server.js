d79c7ca7684b483814ed1ef2f8789519d36f169a
ImeneDehimi <imenedehimi8@gmail.com>
2024-05-25 12:54:50 +0100

    ..

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-require('dotenv').config();
+require('dotenv').config()
 // imports
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const routes = require("./router/index");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const routes = require("./router/index")
 
-const http = require('http');
 const socketIo = require('socket.io');
 
-app.use(express.json());
-app.use(cors());
 
-// Create HTTP server
-const server = http.createServer(app);
+app.use(express.json())
+app.use(cors())
 
-// Route
-app.use("/", routes);
-app.get('/', (req, res) => {
-  res.send({ message: "Welcome to the API" });
-});
 
-// Socket.IO
-const io = socketIo(server, {
+// route
+app.use("/" ,routes) 
+app.get('/', (req, res)=>{
+  res.send({message : "Welcome to the API"})
+})
+
+// server init::
+const port = process.env.PORT || 5000
+const server = app.listen(port , ()=> console.log(`server running on port ${port}`))
+
+// connecting to database
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connected to database!"))
+  .catch((err) => console.error("Error connecting to database:", err));
+
+  // socket
+
+
+const io = require("socket.io")(server, {
   cors: {
     origin: "https://handyman-home-services.netlify.app",
   },
@@ -31,39 +41,30 @@ const io = socketIo(server, {
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-  // Add new user
+  // add new User
   socket.on("new-user-add", (newUserId) => {
-    // If user is not added previously
+    // if user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
     }
-    // Send all active users to new user
+    // send all active users to new user
     io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
-    // Remove user from active users
+    // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    // Send all active users to all users
+    // send all active users to all users
     io.emit("get-users", activeUsers);
   });
 
-  // Send message to a specific user
+  // send message to a specific user
   socket.on("send-message", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
     if (user) {
-      io.to(user.socketId).emit("receive-message", data);
+      io.to(user.socketId).emit("recieve-message", data);
     }
   });
 });
 
-// Server initialization
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Server running on port ${port}`));
-
-// Connecting to database
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log("Connected to database!"))
-  .catch((err) => console.error("Error connecting to database:", err));
